9fb9dc6c29442245aea474d6316c5f8fd022ef99
abdulvahapbilen <abdulvahap.bilen@bil.omu.edu.tr>
2014-12-02 07:11:08 +0200

    Create replace.go

---
+++ replace.go
@@ -0,0 +1,37 @@
+package main
+
+ import (
+     "fmt"
+     "strings"
+ )
+
+ func main() {
+	var str string
+	var ilk int
+	var son int
+	var i int
+	fmt.Scanf("%s", str)
+	str_split := strings.Split(str, "")
+
+	for ilk = 0; ilk < len(str_split); ilk++ { 
+		if string(str_split[ilk]) != "_" {break}
+	}
+	
+	for son = len(str_split)-1; son > 0; son-- {  
+		if string(str_split[son]) != "_" {break}
+	}
+	
+	for i = ilk+1; i < son; i++ {
+		if  string(str_split[i]) == "_"{
+			str_split[i] = " "
+		}
+	}
+	
+	str = ""
+	for _, v := range str_split {     //tekrardan stringe çevirme
+		str += string(v)
+	}
+	
+	fmt.Println(string(str))
+
+ }
